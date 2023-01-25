e9cece823a46b3659c42fd899d5e955f0a354d8f
takadmin00 <tarik.okaben@hotmail.com>
2023-01-25 20:56:17 +0100

    test connexion string

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("register");
 });
 
 module.exports = router;
